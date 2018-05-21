cd0d1a99fc5cc49fb5cf20e42367dd641649dd86
Heath Burton <rubickdev@gmail.com>
2018-05-21 21:55:17 +1000

    completed skeleton layout

--- app.js
+++ app.js
@@ -8,3 +8,23 @@ for (let j = 0; j < colourPickerButtons.length; j++){
 function playerClick(){
   console.log("registered click");
 };
+
+function playRed(){
+  var audio1 = document.getElementById('audio-red');
+  audio1.play();
+}
+
+function playBlue(){
+  var audio2 = document.getElementById('audio-blue');
+  audio2.play();
+}
+
+function playGreen(){
+  var audio3 = document.getElementById('audio-green');
+  audio3.play();
+}
+
+function playYellow(){
+  var audio4 = document.getElementById('audio-yellow');
+  audio4.play();
+}
